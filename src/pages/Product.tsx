e738b4e583b7ed8f19cb12fd6484c22fcdc0a776
MarkusIngerslev <markusolsen2004@gmail.com>
2024-05-02 19:51:05 +0200

    Første udkast til at få sortingers- og filterings knapper til at virke

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -32,54 +32,96 @@ function Product() {
                         </div>
                     </div>
                 </div>
+
                 {/* Ny container til næste sektion */}
                 <div className="container" style={{ marginTop: "calc(9/21 * 105vw)" }}>
-                    <div className="row">
-                        <div className="col justify-content-between">
-                            <div>
-                                <ProductDropDown
-                                    title="PRIS"
-                                    items={[
-                                        { label: "Kr 0 - 999", value: "0-999", href: "#" },
-                                        { label: "Kr 1000 - 1999", value: "1000-1999", href: "#" },
-                                        { label: "Kr 2000 - 3999", value: "2000-3999", href: "#" },
-                                        { label: "Kr 4000+", value: "4000+", href: "#" },
-                                    ]}
-                                />
+                    {/* marginTop er sat til 9/21 dele af bredden på billedet, */}
+                    {/* således at næste container med content kommer frem under billedet */}
 
-                                <ProductDropDown
-                                    title="PRODUKT TYPE"
-                                    items={[
-                                        { label: "Ring", value: "ring", href: "#" },
-                                        { label: "Ørering", value: "earing", href: "#" },
-                                        { label: "Halskæde", value: "necklace", href: "#" },
-                                    ]}
-                                />
-
-                                <ProductDropDown
-                                    title="MATERIALE"
-                                    items={[
-                                        { label: "Guld", value: "gold", href: "#" },
-                                        { label: "Sølv", value: "silver", href: "#" },
-                                        { label: "Platin", value: "platinum", href: "#" },
-                                        { label: "Genanvedt plastik", value: "plastic", href: "#" },
-                                    ]}
-                                />
+                    <nav className="navbar navbar-expand-lg">
+                        <div className="container-fluid">
+                            <div className="row  mx-auto">
+                                <div className="col">
+                                    <button
+                                        className="navbar-toggler"
+                                        type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#navbarTogglerFiltrer"
+                                        aria-controls="navbarTogglerFiltrer"
+                                        aria-expanded="false"
+                                        aria-label="Toggle navigation"
+                                    >
+                                        <span>FILTRER EFTER</span>
+                                    </button>
+                                </div>
+                                <div className="col">
+                                    <button
+                                        className="navbar-toggler"
+                                        type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#navbarTogglerSorting"
+                                        aria-controls="navbarTogglerSorting"
+                                        aria-expanded="false"
+                                        aria-label="Toggle navigation"
+                                    >
+                                        <span>Sorting</span>
+                                    </button>
+                                </div>
                             </div>
-                            <div className="end-0">
-                                <ProductDropDown
-                                    title="SORTER EFTER"
-                                    position=""
-                                    items={[
-                                        { label: "Nyheder", value: "news", href: "#" },
-                                        { label: "Mest populære", value: "most-popular", href: "#" },
-                                        { label: "Pris lav til høj", value: "pric-low-to-high", href: "#" },
-                                        { label: "Pris høj til lav", value: "price-high-to-low", href: "#" },
-                                    ]}
-                                />
+                            <div className="collapse navbar-collapse" id="navbarTogglerFiltrer">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <ProductDropDown
+                                            title="PRIS"
+                                            items={[
+                                                { label: "Kr 0 - 999", value: "0-999", href: "#" },
+                                                { label: "Kr 1000 - 1999", value: "1000-1999", href: "#" },
+                                                { label: "Kr 2000 - 3999", value: "2000-3999", href: "#" },
+                                                { label: "Kr 4000+", value: "4000+", href: "#" },
+                                            ]}
+                                        />
+                                    </li>
+                                    <li className="nav-item">
+                                        <ProductDropDown
+                                            title="PRODUKT TYPE"
+                                            items={[
+                                                { label: "Ring", value: "ring", href: "#" },
+                                                { label: "Ørering", value: "earing", href: "#" },
+                                                { label: "Halskæde", value: "necklace", href: "#" },
+                                            ]}
+                                        />
+                                    </li>
+                                    <li className="nav-item">
+                                        <ProductDropDown
+                                            title="MATERIALE"
+                                            items={[
+                                                { label: "Guld", value: "gold", href: "#" },
+                                                { label: "Sølv", value: "silver", href: "#" },
+                                                { label: "Platin", value: "platinum", href: "#" },
+                                                { label: "Genanvedt plastik", value: "plastic", href: "#" },
+                                            ]}
+                                        />
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="collapse navbar-collapse" id="navbarTogglerSorting">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <ProductDropDown
+                                            title="SORTER EFTER"
+                                            position="justify-content-end"
+                                            items={[
+                                                { label: "Nyheder", value: "news", href: "#" },
+                                                { label: "Mest populære", value: "most-popular", href: "#" },
+                                                { label: "Pris lav til høj", value: "pric-low-to-high", href: "#" },
+                                                { label: "Pris høj til lav", value: "price-high-to-low", href: "#" },
+                                            ]}
+                                        />
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                    </div>
+                    </nav>
                 </div>
             </div>
         </>
