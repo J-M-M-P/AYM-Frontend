5eb1a2fb86b5e6a2d8ba056dabec89d44a62dd64
MarkusIngerslev <markusolsen2004@gmail.com>
2024-05-03 16:15:17 +0200

    Nu fået knapper til at være sammen og se nogenlunde ud ved siden af hinanden

--- src/components/Product/ProductDropDown.tsx
+++ src/components/Product/ProductDropDown.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface DropdownItem {
     label: string;
     value: string;
@@ -5,32 +7,58 @@ interface DropdownItem {
 }
 
 interface DropdownProps {
-    position?: string;
+    group: boolean;
     title: string;
     items: DropdownItem[];
 }
 
-function ProductDropDown({ title, items }: DropdownProps) {
+function ProductDropDown({ title, items, group }: DropdownProps) {
+    const [dropDownGroup] = useState(group);
+
     return (
-        <div className={`btn-group px-3`}>
-            <button
-                type="button"
-                className="btn btn-primary dropdown-toggle"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-            >
-                {title}
-            </button>
-            <ul className="dropdown-menu">
-                {items.map((item, index) => (
-                    <li key={index}>
-                        <a className="dropdown-item" href={item.href}>
-                            <span>{item.label}</span>
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            {(dropDownGroup !== true && (
+                <div className={`btn-group px-3`}>
+                    <button
+                        type="button"
+                        className="btn btn-primary dropdown-toggle"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        {title}
+                    </button>
+                    <ul className="dropdown-menu">
+                        {items.map((item, index) => (
+                            <li key={index}>
+                                <a className="dropdown-item" href={item.href}>
+                                    <span>{item.label}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )) || (
+                <div className={`btn-group w-100 h-100`}>
+                    <button
+                        type="button"
+                        className="btn btn-dark dropdown-toggle "
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        {title}
+                    </button>
+                    <ul className="dropdown-menu end-0">
+                        {items.map((item, index) => (
+                            <li key={index}>
+                                <a className="dropdown-item" href={item.href}>
+                                    <span>{item.label}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </>
     );
 }
 
--- src/components/Product/ProductNavbar.tsx
+++ src/components/Product/ProductNavbar.tsx
@@ -3,103 +3,62 @@ import ProductDropDown from "./ProductDropDown";
 function ProductNavbar() {
     return (
         <>
-            <div className="row">
+            {/* Filter Dropdowns & Off canvas menu when, width < navbar-expand-md */}
+            <div className="row mb-2 px-1">
                 <div className="col px-0">
-                    <nav className="navbar navbar-expand-lg">
-                        <div className="container-fluid px-0">
-                            <div className="row ms-auto me-0">
-                                <button
-                                    className="navbar-toggler"
-                                    type="button"
-                                    data-bs-toggle="offcanvas"
-                                    data-bs-target="#offcanvasNavbar"
-                                    aria-controls="offcanvasNavbar"
-                                    aria-expanded="false"
-                                    aria-label="Toggle navigation"
-                                >
-                                    <span>FILTRER EFTER</span>
-                                </button>
-                            </div>
-                            <div
-                                className="offcanvas offcanvas-start"
-                                tabIndex={-1}
-                                id="offcanvasNavbar"
-                                aria-labelledby="offcanvasNavbarLabel"
-                            >
-                                <div className="offcanvas-body">
-                                    <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
-                                        <li className="nav-item">
-                                            <ProductDropDown
-                                                title="PRIS"
-                                                items={[
-                                                    { label: "Kr 0 - 999", value: "0-999", href: "#" },
-                                                    { label: "Kr 1000 - 1999", value: "1000-1999", href: "#" },
-                                                    { label: "Kr 2000 - 3999", value: "2000-3999", href: "#" },
-                                                    { label: "Kr 4000+", value: "4000+", href: "#" },
-                                                ]}
-                                            />
-                                        </li>
-                                        <li className="nav-item">
-                                            <ProductDropDown
-                                                title="PRODUKT TYPE"
-                                                items={[
-                                                    { label: "Ring", value: "ring", href: "#" },
-                                                    { label: "Ørering", value: "earing", href: "#" },
-                                                    { label: "Halskæde", value: "necklace", href: "#" },
-                                                ]}
-                                            />
-                                        </li>
-                                        <li className="nav-item">
-                                            <ProductDropDown
-                                                title="MATERIALE"
-                                                items={[
-                                                    { label: "Guld", value: "gold", href: "#" },
-                                                    { label: "Sølv", value: "silver", href: "#" },
-                                                    { label: "Platin", value: "platinum", href: "#" },
-                                                    { label: "Genanvedt plastik", value: "plastic", href: "#" },
-                                                ]}
-                                            />
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-                <div className="col px-0">
-                    <nav className="navbar navbar-expand-lg">
-                        <div className="container-fluid px-0">
-                            <div className="row ms-0 me-auto">
-                                <button
-                                    className="navbar-toggler"
-                                    type="button"
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarTogglerSorting"
-                                    aria-controls="navbarTogglerSorting"
-                                    aria-expanded="false"
-                                    aria-label="Toggle navigation"
-                                >
-                                    <span>Sorting</span>
-                                </button>
-                            </div>
-                            <div className="collapse navbar-collapse" id="navbarTogglerSorting">
-                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <nav className="navbar navbar-expand-md py-0 h-100">
+                        <button
+                            className="navbar-toggler w-100 h-100"
+                            type="button"
+                            data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasNavbar"
+                            aria-controls="offcanvasNavbar"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span>FILTRER EFTER</span>
+                        </button>
+
+                        <div
+                            className="offcanvas offcanvas-start"
+                            tabIndex={-1}
+                            id="offcanvasNavbar"
+                            aria-labelledby="offcanvasNavbarLabel"
+                        >
+                            <div className="offcanvas-body">
+                                <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
                                     <li className="nav-item">
                                         <ProductDropDown
-                                            title="SORTER EFTER"
+                                            title="PRIS"
+                                            group={false}
                                             items={[
-                                                { label: "Nyheder", value: "news", href: "#" },
-                                                { label: "Mest populære", value: "most-popular", href: "#" },
-                                                {
-                                                    label: "Pris lav til høj",
-                                                    value: "pric-low-to-high",
-                                                    href: "#",
-                                                },
-                                                {
-                                                    label: "Pris høj til lav",
-                                                    value: "price-high-to-low",
-                                                    href: "#",
-                                                },
+                                                { label: "Kr 0 - 999", value: "0-999", href: "#" },
+                                                { label: "Kr 1000 - 1999", value: "1000-1999", href: "#" },
+                                                { label: "Kr 2000 - 3999", value: "2000-3999", href: "#" },
+                                                { label: "Kr 4000+", value: "4000+", href: "#" },
+                                            ]}
+                                        />
+                                    </li>
+                                    <li className="nav-item">
+                                        <ProductDropDown
+                                            title="PRODUKT TYPE"
+                                            group={false}
+                                            items={[
+                                                { label: "Ring", value: "ring", href: "#" },
+                                                { label: "Ørering", value: "earing", href: "#" },
+                                                { label: "Halskæde", value: "necklace", href: "#" },
+                                            ]}
+                                        />
+                                    </li>
+                                    <li className="nav-item">
+                                        <ProductDropDown
+                                            title="MATERIALE"
+                                            group={false}
+                                            items={[
+                                                { label: "Guld", value: "gold", href: "#" },
+                                                { label: "Sølv", value: "silver", href: "#" },
+                                                { label: "Platin", value: "platinum", href: "#" },
+                                                { label: "Genanvedt plastik", value: "plastic", href: "#" },
                                             ]}
                                         />
                                     </li>
@@ -107,6 +66,28 @@ function ProductNavbar() {
                             </div>
                         </div>
                     </nav>
+                </div>
+
+                {/* Sorting Dropdown button */}
+                <div className="col px-0">
+                    <ProductDropDown
+                        title="SORTER EFTER"
+                        group={true}
+                        items={[
+                            { label: "Nyheder", value: "news", href: "#" },
+                            { label: "Mest populære", value: "most-popular", href: "#" },
+                            {
+                                label: "Pris lav til høj",
+                                value: "pric-low-to-high",
+                                href: "#",
+                            },
+                            {
+                                label: "Pris høj til lav",
+                                value: "price-high-to-low",
+                                href: "#",
+                            },
+                        ]}
+                    />
                 </div>
             </div>
         </>
