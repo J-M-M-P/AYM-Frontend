57a2578a968b918ff0cad15d9e59431c9df0c291
MarkusIngerslev <markusolsen2004@gmail.com>
2024-05-06 22:50:42 +0200

    Ændre homepage ImageSlider til at være en Carousel fra bootstrap

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,37 +1,37 @@
-import { useState, useEffect } from "react";
 import testImages from "../tests/testHomePageImages";
 
-// const images = [
-//     "https://th.bing.com/th/id/OIP.u72WQ739A7q0yz4-DkCklgHaEj?w=291&h=180&c=7&r=0&o=5&pid=1.7",
-//     "https://th.bing.com/th/id/OIP.RE9k9rqVorwokoPI9h6UrAHaJJ?w=174&h=215&c=7&r=0&o=5&pid=1.7",
-//     "https://th.bing.com/th/id/OIP.E7vAG94YruqJSuitpiFQhAHaEA?w=319&h=180&c=7&r=0&o=5&pid=1.7",
-//     "https://th.bing.com/th/id/OIP.WcM6Of7nRCTdmaqnrJYvQgHaEK?w=293&h=180&c=7&r=0&o=5&pid=1.7",
-//     "https://th.bing.com/th/id/OIP.23GlTXVkWQzS0S2yJXGAXAHaEK?w=270&h=180&c=7&r=0&o=5&pid=1.7",
-// ];
-
 export default function ImageSlider() {
-    const [index, setIndex] = useState(0);
-    const nextImage = testImages.map((image) => image.imgSrc);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIndex((prevIndex) => (prevIndex === nextImage.length - 1 ? 0 : prevIndex + 1));
-        }, 8000);
-        return () => clearInterval(interval);
-    }, [nextImage.length]);
-
     return (
-        <div className="container-fluid p-0">
-            <div className="row m-0">
-                <div className="col p-0">
-                    <img
-                        src={nextImage[index]}
-                        alt={`Slide ${index + 1}`}
-                        className="img-fluid"
-                        style={{ maxHeight: "calc(100vh - 56px)", width: "200vh" }}
-                    />
+        <>
+            <div className="carousel slide" id="imageCarouselAutoPlaying" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    {testImages.map((image, index) => (
+                        <>
+                            <div className={`carousel-item ${index == 0 && "active"}`}>
+                                <img src={image.imgSrc} alt="" className="d-block w-100" />
+                            </div>
+                        </>
+                    ))}
                 </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#imageCarouselAutoPlaying"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#imageCarouselAutoPlaying"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-        </div>
+        </>
     );
 }
