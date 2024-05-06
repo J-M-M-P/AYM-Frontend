ba8ce64c5a77459bba0fb7639139492400c70549
MarkusIngerslev <markusolsen2004@gmail.com>
2024-05-06 22:20:25 +0200

    Tilføjet nye billeder to homepage, med bedre opløsning til at fylde side

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import testImages from "../tests/testHomePageImages";
 
-const images = [
-  "https://th.bing.com/th/id/OIP.u72WQ739A7q0yz4-DkCklgHaEj?w=291&h=180&c=7&r=0&o=5&pid=1.7",
-  "https://th.bing.com/th/id/OIP.RE9k9rqVorwokoPI9h6UrAHaJJ?w=174&h=215&c=7&r=0&o=5&pid=1.7",
-  "https://th.bing.com/th/id/OIP.E7vAG94YruqJSuitpiFQhAHaEA?w=319&h=180&c=7&r=0&o=5&pid=1.7",
-  "https://th.bing.com/th/id/OIP.WcM6Of7nRCTdmaqnrJYvQgHaEK?w=293&h=180&c=7&r=0&o=5&pid=1.7",
-  "https://th.bing.com/th/id/OIP.23GlTXVkWQzS0S2yJXGAXAHaEK?w=270&h=180&c=7&r=0&o=5&pid=1.7"
-];
+// const images = [
+//     "https://th.bing.com/th/id/OIP.u72WQ739A7q0yz4-DkCklgHaEj?w=291&h=180&c=7&r=0&o=5&pid=1.7",
+//     "https://th.bing.com/th/id/OIP.RE9k9rqVorwokoPI9h6UrAHaJJ?w=174&h=215&c=7&r=0&o=5&pid=1.7",
+//     "https://th.bing.com/th/id/OIP.E7vAG94YruqJSuitpiFQhAHaEA?w=319&h=180&c=7&r=0&o=5&pid=1.7",
+//     "https://th.bing.com/th/id/OIP.WcM6Of7nRCTdmaqnrJYvQgHaEK?w=293&h=180&c=7&r=0&o=5&pid=1.7",
+//     "https://th.bing.com/th/id/OIP.23GlTXVkWQzS0S2yJXGAXAHaEK?w=270&h=180&c=7&r=0&o=5&pid=1.7",
+// ];
 
 export default function ImageSlider() {
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
+    const nextImage = testImages.map((image) => image.imgSrc);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, 8000);
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex === nextImage.length - 1 ? 0 : prevIndex + 1));
+        }, 8000);
+        return () => clearInterval(interval);
+    }, [nextImage.length]);
 
-  return (
-    <div className="container-fluid p-0">
-      <div className="row">
-        <div className="col">
-          <img src={images[index]} alt={`Slide ${index + 1}`} className="img-fluid" style={{ maxHeight: "calc(100vh - 56px)", width: "200vh"}} />
+    return (
+        <div className="container-fluid p-0">
+            <div className="row m-0">
+                <div className="col p-0">
+                    <img
+                        src={nextImage[index]}
+                        alt={`Slide ${index + 1}`}
+                        className="img-fluid"
+                        style={{ maxHeight: "calc(100vh - 56px)", width: "200vh" }}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
