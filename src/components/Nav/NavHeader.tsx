f7ae1928aebb2c384dc073c42a0fa927cb0cb118
MarkusIngerslev <markusolsen2004@gmail.com>
2024-05-16 19:22:35 +0200

    Lavet første opsætning af ny navbar, med offcanvas funktion, ved mindre skærm

--- src/components/Nav/NavHeader.tsx
+++ src/components/Nav/NavHeader.tsx
@@ -1,44 +1,58 @@
 import { NavLink } from "react-router-dom";
 import { FaSearch, FaUser, FaShoppingBasket, FaShoppingBag } from "react-icons/fa";
+import "./NavStyling.css";
+
+// React-Bootstrap
+import { Navbar, Nav, Form, Container, Offcanvas } from "react-bootstrap";
 
 export default function NavHeader() {
-  return (
-    <>
-      <nav className="navbar bg-dark border-bottom navbar-expand-lg border-body" data-bs-theme="dark">
-        <div className="container-fluid" style={{ margin: "0", padding: "0" }}>
-          <div className="container d-flex justify-content-between align-items-center" style={{ margin: "0 10px" }}>
-            <div className="d-flex align-items-center">
-              <NavLink to="/shop" className="text-light me-2 text-decoration-none">
-                <FaShoppingBag />
-              </NavLink>
-              <NavLink to="/shop" className="text-light text-decoration-none">
-                Shop
-              </NavLink>
-            </div>
+    return (
+        <>
+            <Container>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Container>
+                        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+                        <Navbar.Offcanvas
+                            id="offcanvasNavbar-expand-lg"
+                            aria-labelledby="offcanvasNavbar-expand-lg"
+                            className="bg-dark"
+                            placement="start"
+                        >
+                            <Offcanvas.Header closeButton></Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }}>
+                                    <NavLink to="/shop" className="text-light me-2 text-decoration-none">
+                                        <FaShoppingBag />
+                                    </NavLink>
+                                    <NavLink to="/shop" className="text-light text-decoration-none">
+                                        Shop
+                                    </NavLink>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                    <NavLink
+                        className="navbar-brand"
+                        to="/"
+                        style={{ fontWeight: "bold", fontSize: "24px", color: "#fff" }}
+                    >
+                        AYM
+                    </NavLink>
+                    <Form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-light" type="submit">
+                            <FaSearch />
+                        </button>
+                    </Form>
+                    <NavLink to="/login" className="text-light me-2">
+                        <FaUser />
+                    </NavLink>
 
-            <NavLink className="navbar-brand mx-auto" to="/" style={{ fontWeight: "bold", fontSize: "24px", color: "#fff" }}>
-              AYM
-            </NavLink>
-
-            <div className="d-flex align-items-center" style={{ marginLeft: "-200px", marginRight: "0px" }}>
-              <form className="d-flex me-2">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-light" type="submit">
-                  <FaSearch />
-                </button>
-              </form>
-
-              <NavLink to="/login" className="text-light me-2">
-                <FaUser />
-              </NavLink>
-
-              <NavLink to="/basket" className="text-light">
-                <FaShoppingBasket />
-              </NavLink>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+                    <NavLink to="/basket" className="text-light">
+                        <FaShoppingBasket />
+                    </NavLink>
+                </Navbar>
+            </Container>
+        </>
+    );
 }
